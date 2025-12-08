a6fea6439a7ad4fc5239b150135f425d7d7f367b
webboom0 <webboom@naver.com>
2025-12-08 09:30:49 +0900

    fix(MyInfoScreen): 하단 SAFE_AREA 색상 변경 및 알림 배지 표시 로직 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -150,7 +150,7 @@ export const FORM_STYLES = {
 
 export const SAFE_AREA = {
   background: '#ffffff',
-  bottomBackground: '#fafafa',
+  bottomBackground: '#ffffff',
 };
 
 export const HEADER = {
