c067bd5c61afcb15435272a686f29beb300e0e84
sungjius <sungji.us@gmail.com>
2025-12-11 17:11:57 +0900

    feat(AdWalletScreen, locales): 퀘스트 기능 추가 및 다국어 지원 강화

--- src/locales/id/screens/adWallet.ts
+++ src/locales/id/screens/adWallet.ts
@@ -1,11 +1,13 @@
 export default {
   title: 'AD',
   pending: 'Sedang Ditinjau',
+  quest: 'Quest',
   settled: 'Selesai',
   conditionNotMet: 'Kondisi Tidak Terpenuhi',
   expectedAmount: 'Jumlah yang Diharapkan',
   confirmedAmount: 'Jumlah yang Dikonfirmasi',
   expectedAdRevenue: 'Pendapatan Iklan yang Diharapkan',
   adRevenueSettlement: 'Penyelesaian Pendapatan Iklan',
+  rewardAmount: 'Jumlah Hadiah',
 };
 
