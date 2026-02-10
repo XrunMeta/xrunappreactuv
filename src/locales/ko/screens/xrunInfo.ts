9e62b8d21239247d7a06859a9d2c00078bb4792a
Doongiho <happyluck0426@gmail.com>
2026-02-10 16:18:04 +0900

    기프티콘 리스트 불러오는 api 추가가

---
+++ src/locales/ko/screens/xrunInfo.ts
@@ -0,0 +1,7 @@
+export default {
+  giftishowTitle: '기프티쇼 비즈',
+  bizMoneyBalance: '비즈머니 잔액',
+  productsCoupons: '상품/쿠폰',
+  dailyLimit: '테스트 환경 일일 발송 한도 1,000건',
+  couponSendRequest: '쿠폰 발송 요청',
+};
