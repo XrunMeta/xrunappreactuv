18f435b9a49dfff0fc3f82f4105f3ff5eb13b73e
Doongiho <happyluck0426@gmail.com>
2026-06-23 18:54:15 +0900

    style(shop-detail): 이미지 모서리 8 → 16 (잘 보이게)

--- src/screens/ShopProductDetailScreen.tsx
+++ src/screens/ShopProductDetailScreen.tsx
@@ -1507,7 +1507,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 200,
 
-        borderRadius: 8,
+        borderRadius: 16,
         alignItems: 'center',
         justifyContent: 'center',
         overflow: 'hidden',
